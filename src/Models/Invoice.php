e3ffc755fb6d79b9e95ff7cf84612f8a1816bdd4
Mohammad Mortazavi <s.mohammad027@live.com>
2023-08-27 16:53:42 +0330

    creating event added for Invoice model;

--- src/Models/Invoice.php
+++ src/Models/Invoice.php
@@ -5,12 +5,40 @@
 	use Illuminate\Database\Eloquent\Model;
 	use Illuminate\Database\Eloquent\Relations\HasMany;
 
+	/**
+	 * @property int $id
+	 * @property int $number
+	 * @property int $token
+	 * @property int $transaction_id
+	 */
 	class Invoice extends Model {
 
+		/**
+		 * The attributes that are mass assignable.
+		 *
+		 * @var array<string>
+		 */
 		protected $fillable = [
-			'number'
+			'number',
+			'token',
+			'transaction_id',
 		];
 
+		/**
+		 * Perform any actions required after the model boots.
+		 *
+		 * @return void
+		 */
+		protected static function booted() {
+			self::creating( fn( self $model ) => $model->number = generate_unique_invoice_number() );
+		}
+
+
+		/**
+		 * Relationship's definition with Invoicable
+		 *
+		 * @return HasMany
+		 */
 		public function items(): HasMany {
 			return $this->hasMany( Invoicable::class )->latest();
 		}
