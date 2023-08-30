4c389cff503c900c38c435519354043ce504f8b4
Mohammad Mortazavi <s.mohammad027@live.com>
2023-08-30 11:51:04 +0330

    Events created;

---
+++ src/Events/TransactionIdReceived.php
@@ -0,0 +1,21 @@
+<?php
+
+	namespace Hans\Lyra\Events;
+
+	use Hans\Lyra\Models\Invoice;
+	use Illuminate\Broadcasting\InteractsWithSockets;
+	use Illuminate\Foundation\Events\Dispatchable;
+	use Illuminate\Queue\SerializesModels;
+
+	class TransactionIdReceived {
+		use Dispatchable, InteractsWithSockets, SerializesModels;
+
+		/**
+		 * Create a new event instance.
+		 */
+		public function __construct(
+			public Invoice $invoice,
+			public string $transId
+		) {
+		}
+	}
