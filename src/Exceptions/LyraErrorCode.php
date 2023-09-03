6de3f13cebe59bda69a5154d6dfa75aeae17f0e3
Mohammad Mortazavi <s.mohammad027@live.com>
2023-09-03 09:16:58 +0330

    LyraException created;

---
+++ src/Exceptions/LyraErrorCode.php
@@ -0,0 +1,7 @@
+<?php
+
+	namespace Hans\Lyra\Exceptions;
+
+	class LyraErrorCode {
+		public const DEFAULT_GATEWAY_NOT_FOUNT = 'LyraEC0';
+	}
---
+++ src/Exceptions/LyraException.php
@@ -0,0 +1,25 @@
+<?php
+
+	namespace Hans\Lyra\Exceptions;
+
+	use Exception;
+	use Throwable;
+
+	class LyraException extends Exception {
+		private int|string $errorCode;
+
+		public function __construct(string $message, int|string $errorCode, int $responseCode = 500, Throwable $previous = null)
+		{
+			parent::__construct($message, $responseCode, $previous);
+			$this->errorCode = $errorCode;
+		}
+
+		public static function make( string $message, int|string $errorCode, int $responseCode = 500, Throwable $previous = null ):self {
+			return new self( $message,$errorCode, $responseCode, $previous );
+		}
+
+		public function getErrorCode(): int|string
+		{
+			return $this->errorCode;
+		}
+	}
