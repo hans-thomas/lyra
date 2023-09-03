058b8244bd79cd856b7e84716a96cce01846e629
Mohammad Mortazavi <s.mohammad027@live.com>
2023-09-03 19:07:16 +0330

    new error codes added;

--- src/Exceptions/LyraErrorCode.php
+++ src/Exceptions/LyraErrorCode.php
@@ -2,7 +2,9 @@
 
 namespace Hans\Lyra\Exceptions;
 
-    class LyraErrorCode
-    {
-        public const DEFAULT_GATEWAY_NOT_FOUNT = 'LyraEC0';
-    }
+class LyraErrorCode
+{
+    public const GATEWAY_CLASS_NOT_FOUNT = 'LyraEC0';
+    public const WRONG_GATEWAY_CLASS_SELECTED = 'LyraEC1';
+    public const FAILED_TO_VERIFYING = 'LyraEC2';
+}
