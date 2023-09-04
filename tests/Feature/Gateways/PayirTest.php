4f51d379933da769803412ecdbdd0ae06dd7b8b1
Mohammad Mortazavi <s.mohammad027@live.com>
2023-09-04 09:54:45 +0330

    Update PayirTest.php

--- tests/Feature/Gateways/PayirTest.php
+++ tests/Feature/Gateways/PayirTest.php
@@ -2,80 +2,88 @@
 
 namespace Hans\Lyra\Tests\Feature\Gateways;
 
-    use Hans\Lyra\Gateways\Payir;
-    use Hans\Lyra\Tests\TestCase;
-    use Illuminate\Support\Str;
+use Hans\Lyra\Exceptions\LyraException;
+use Hans\Lyra\Facades\Lyra;
+use Hans\Lyra\Gateways\Payir;
+use Hans\Lyra\Tests\TestCase;
+use Illuminate\Support\Str;
 
-    class PayirTest extends TestCase
+class PayirTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function request(): void
     {
-        /**
-         * @test
-         *
-         * @return void
-         */
-        public function request(): void
-        {
-            $instance = new Payir(10000, 'sandbox');
+        Lyra::setGateway(Payir::class, 10000, 'sandbox');
 
-            self::assertIsString($instance->request());
-        }
-
-        /**
-         * @test
-         *
-         * @return void
-         */
-        public function requestWithInvalidSettings(): void
-        {
-            $instance = new Payir(1, 'sandbox');
-
-            $this->expectExceptionMessage($instance->errorsList()[-12]);
-            self::assertIsString($instance->request());
-        }
-
-        /**
-         * @test
-         *
-         * @return void
-         */
-        public function pay(): void
-        {
-            $instance = new Payir(10000, 'sandbox');
-
-            self::assertIsString($url = $instance->pay());
-            $response = $this->client->get($url)->getBody()->getContents();
-            self::assertStringContainsString('درگاه تست Pay.ir', $response);
-        }
-
-        /**
-         * @test
-         *
-         * @return void
-         */
-        public function verifyOnSuccess(): void
-        {
-            $instance = new Payir(10000, 'sandbox');
-            $url = $instance->pay();
-            $token = Str::afterLast($url, '/');
-
-            request()->merge(['status' => 1, 'token' => $token]);
-
-            self::assertTrue($instance->verify());
-        }
-
-        /**
-         * @test
-         *
-         * @return void
-         */
-        public function verifyOnFailed(): void
-        {
-            $instance = new Payir(10000, 'sandbox');
-            $url = $instance->pay();
-            $token = Str::afterLast($url, '/');
-
-            request()->merge(['status' => 0, 'token' => $token]);
-
-            self::assertFalse($instance->verify());
-        }
+        self::assertIsString(Lyra::pay(1)->getRedirectUrl());
     }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function requestWithInvalidSettings(): void
+    {
+        Lyra::setGateway(Payir::class, 1, 'sandbox');
+        $instance = new Payir;
+
+        $this->expectExceptionMessage($instance->errorsList()[-12]);
+        self::assertIsString(Lyra::pay(1));
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function pay(): void
+    {
+        Lyra::setGateway(Payir::class, 10000, 'sandbox');
+
+        self::assertIsString($url = Lyra::pay(10000)->getRedirectUrl());
+        $response = $this->client->get($url)->getBody()->getContents();
+        self::assertStringContainsString('درگاه تست Pay.ir', $response);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function verifyOnSuccess(): void
+    {
+        Lyra::setGateway(Payir::class, 10000, 'sandbox');
+
+        $url = Lyra::pay(10000)->getRedirectUrl();
+        $token = Str::afterLast($url, '/');
+        request()->merge(['status' => 1, 'token' => $token]);
+
+        self::assertTrue(Lyra::verify());
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function verifyOnFailed(): void
+    {
+        Lyra::setGateway(Payir::class, 10000, 'sandbox');
+
+        $url = Lyra::pay(10000)->getRedirectUrl();
+        $token = Str::afterLast($url, '/');
+        request()->merge(['status' => 0, 'token' => $token]);
+
+        $invoice = Lyra::getInvoice();
+
+        $this->expectException(LyraException::class);
+        $this->expectExceptionMessage("Verifying Invoice #{$invoice->number} failed!");
+
+        Lyra::verify();
+    }
+}
