bbf1b84ec91cf2e43b2a3a6f9ca4faa857f94723
Mohammad Mortazavi <s.mohammad027@live.com>
2023-09-04 14:17:29 +0330

    new error code added;

--- src/Exceptions/LyraErrorCode.php
+++ src/Exceptions/LyraErrorCode.php
@@ -8,4 +8,5 @@ class LyraErrorCode
     public const WRONG_GATEWAY_CLASS_SELECTED = 'LyraEC1';
     public const FAILED_TO_VERIFYING = 'LyraEC2';
     public const TOKEN_MISMATCHED = 'LyraEC3';
+    public const AMOUNT_NOT_PASSED = 'LyraEC4';
 }
