06e42377dd04559754673790950ef3224220922a
Mohammad Mortazavi <s.mohammad027@live.com>
2023-09-18 14:47:45 +0330

    Offline is added to Lyra facade;

--- src/Facades/Lyra.php
+++ src/Facades/Lyra.php
@@ -2,6 +2,7 @@
 
 namespace Hans\Lyra\Facades;
 
+use Hans\Lyra\LyraOfflineService;
 use Hans\Lyra\LyraService;
 use Hans\Lyra\Models\Invoice;
 use Illuminate\Http\RedirectResponse;
@@ -9,26 +10,37 @@ use Illuminate\Support\Facades\Facade;
 use RuntimeException;
 
 /**
- * @method static LyraService      pay(int $amount=null, string $mode=null)
+ * @method static LyraService      pay(int $amount = null, string $mode = null)
  * @method static string           getRedirectUrl()
  * @method static RedirectResponse redirect()
  * @method static LyraService      setGateway(string $gateway, int $amount = null, string $mode = null)
- * @method static bool             verify(int $amount=null, string $mode=null)
+ * @method static bool             verify(int $amount = null, string $mode = null)
  * @method static Invoice          getInvoice()
  *
  * @see LyraService
  */
 class Lyra extends Facade
 {
+    private static LyraOfflineService $offline_service;
+
     /**
      * Get the registered name of the component.
      *
+     * @return string
      * @throws RuntimeException
      *
-     * @return string
      */
     protected static function getFacadeAccessor()
     {
         return 'lyra-service';
+    }
+
+    public static function offline(): LyraOfflineService
+    {
+        if (isset(self::$offline_service)) {
+            return self::$offline_service;
+        }
+
+        return self::$offline_service = new LyraOfflineService();
     }
 }
